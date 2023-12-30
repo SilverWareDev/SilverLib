dc235fd8207dc5d87a59e7731a594da22dbf2893
KitchenBoy <coleton.trenton@outlook.com>
2023-12-29 20:25:27 -0600

    Initial Push

---
+++ Core/src/Main.cs
@@ -0,0 +1,32 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilverLib
+{
+    public class Main : MelonMod
+    {
+        public override void OnEarlyInitializeMelon()
+        {
+            base.OnEarlyInitializeMelon();
+        }
+
+        public override void OnInitializeMelon()
+        {
+            base.OnInitializeMelon();
+        }
+
+        public override void OnLateInitializeMelon()
+        {
+            base.OnLateInitializeMelon();
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using SilverLib;
+
+// Information about a mod is controlled through the following set of
+// attributes. Change these attribute values to modify the information
+// associated with a mod.
+[assembly: MelonInfo(typeof(SilverLib.Main), "SilverLib", 1, 0, 0, "1.0.0", "SilverWare")]
+[assembly: MelonGame("$developer$", "$game$")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SilverLib")]
+[assembly: AssemblyDescription("A general library for all things SilverWare!")]
+[assembly: AssemblyCompany("SilverWare")]
+[assembly: AssemblyProduct("SilverLib")]
